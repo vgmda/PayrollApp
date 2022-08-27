7e7934f50a78b515d3ac6391747cfe8f29a39389
vgmda <vasilegancin@gmail.com>
2022-08-27 23:12:06 +0100

    Working on manager class

--- Data/Manager.cs
+++ Data/Manager.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 namespace PayrollApp.Data;
 
+/// <summary>
+/// The Manager class contains information about each manager in the company.
+/// It also provides a method for calculating basic pay.
+/// It inherits the properties of the parent class. (Staff)
+/// </summary>
+
 public class Manager
 {
+
+    // Fields
+
+
+
+    // Properties
+
+    // Constructor
     public Manager()
     {
     }
+
+    // Methods
 }
 
--- Program.cs
+++ Program.cs
@@ -1,11 +1,20 @@
 ﻿
+using PayrollApp.Data;
+
 namespace PayrollApp;
 
 class Program
 {
     private static void Main(string[] args)
     {
-        // Console.WriteLine("Test");
+
+
+        //Staff s1 = new Staff("Vasile", 5);
+
+        //s1.CalculatePay();
+
+        //Console.WriteLine(s1.ToString());
+
 
         Console.ReadLine();
     }

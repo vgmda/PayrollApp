8c47443b27a201165009abb2ee725971fa748fc9
vgmda <vasilegancin@gmail.com>
2022-08-28 00:18:56 +0100

    Working on admin class

--- Data/Admin.cs
+++ Data/Admin.cs
@@ -1,10 +1,28 @@
 ﻿using System;
 namespace PayrollApp.Data;
 
-public class Admin
+/// <summary>
+/// The Admin class contains information about each admin in the company.
+/// It also provides a method for calculating basic pay.
+/// It inherits the properties of the parent class. (Staff)
+/// </summary>
+
+public class Admin : Staff
 {
-    public Admin()
+
+    // Fields
+    private const float overtimeRate = (float)15.50;
+    private const float adminHourlyRate = 30;
+
+    // Properties
+    public float Overtime { get; private set; }
+
+    // Constructor
+    public Admin(string name) : base(name, adminHourlyRate)
     {
+        // Call the parent constructor and pass the two parameters (name & managerHourlyRate)
     }
+
+    // Methods
 }
 

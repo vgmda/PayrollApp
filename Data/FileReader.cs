4d526850bfb39dd9e7b4191ce55bd21360f21b08
vgmda <vasilegancin@gmail.com>
2022-08-28 01:37:23 +0100

    FileReader class completed

--- Data/FileReader.cs
+++ Data/FileReader.cs
@@ -9,12 +9,46 @@ namespace PayrollApp.Data;
 
 public class FileReader
 {
+
     public List<Staff> ReadFile()
     {
         List<Staff> myStaff = new List<Staff>();
+
+        // Create an array of two elements, will add values later (Name and Position)
         string[] result = new string[2];
-        string path = "staff.txt";
-        string[] separator = { ", " };
+        string path = @"./staff.txt";
+        //string[] separator = { ", " };
+
+        if (File.Exists(path))
+        {
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    result = sr.ReadLine().Split(", ");
+
+                    if (result[1] == "Manager")
+                    {
+                        Manager m;
+                        m = new Manager(result[0]);
+                        myStaff.Add(m);
+                    }
+                    if (result[1] == "Admin")
+                    {
+                        Admin a;
+                        a = new Admin(result[0]);
+                        myStaff.Add(a);
+                    }
+                }
+                sr.Close();
+            }
+        }
+        else
+        {
+            Console.WriteLine("ERROR: File(s) does not exist");
+        }
+
+        return myStaff;
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,22 +7,28 @@ class Program
 {
     private static void Main(string[] args)
     {
-        Staff s1 = new Staff("Vasile", 5);
-        Manager m1 = new Manager("Vasile");
-        Admin a1 = new Admin("Vasile");
+        //Staff s1 = new Staff("Vasile", 5);
+        //Manager m1 = new Manager("Vasile");
+        //Admin a1 = new Admin("Vasile");
 
-        s1.HoursWorked = 200;
-        m1.HoursWorked = 200;
-        a1.HoursWorked = 200;
+        //s1.HoursWorked = 200;
+        //m1.HoursWorked = 200;
+        //a1.HoursWorked = 200;
 
-        s1.CalculatePay();
-        m1.CalculatePay();
-        a1.CalculatePay();
+        //s1.CalculatePay();
+        //m1.CalculatePay();
+        //a1.CalculatePay();
 
-        Console.WriteLine(s1.ToString());
-        Console.WriteLine(m1.ToString());
-        Console.WriteLine(a1.ToString());
+        //Console.WriteLine(s1.ToString());
+        //Console.WriteLine(m1.ToString());
+        //Console.WriteLine(a1.ToString());
+
+        FileReader f = new FileReader();
+
+        f.ReadFile();
 
         Console.ReadLine();
+
+
     }
 }

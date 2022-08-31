20e638b55b2ec8149c7572710d29cc4d509a641d
vgmda <vasilegancin@gmail.com>
2022-08-31 18:01:17 +0100

    New file

---
+++ Data/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace PayrollApp.Data;
+
+/// <summary>
+/// The Employee class contains information about each employee in the company.
+/// It also provides a method for calculating basic pay.
+/// It inherits the properties of the parent class. (Staff)
+/// </summary>
+
+public class Employee : Staff
+{
+    public Employee()
+    {
+    }
+}
+
